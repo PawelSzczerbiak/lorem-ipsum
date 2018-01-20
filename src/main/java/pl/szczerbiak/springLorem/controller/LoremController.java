abeb651e8b495436a9cd45a67acdb4639dc27232
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-01-20 19:15:56 +0100

    validation added

--- src/main/java/pl/szczerbiak/springLorem/controller/LoremController.java
+++ src/main/java/pl/szczerbiak/springLorem/controller/LoremController.java
@@ -2,22 +2,38 @@ package pl.szczerbiak.springLorem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.szczerbiak.springLorem.model.LoremGenerator;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoremController {
 
+    // generate
     @GetMapping("/")
-    public String generateText(){
+    public String generateText(LoremGenerator loremGenerator){
         return "generate";
     }
 
+    // validate
+    @PostMapping("/")
+    public String checkProductInfo(@Valid LoremGenerator loremGenerator, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "generate";
+        }
+        redirectAttributes.addFlashAttribute("text", loremGenerator.generateText());
+        return "redirect:/result"; // maping onto /result
+    }
+
+    // show
     @GetMapping("/result")
-    public String showResults(@RequestParam int n, @RequestParam String type,  ModelMap modelMap){
-        modelMap.addAttribute("text", LoremGenerator.generateText(n, type));
-        return "result";
+    public String showResults(){
+        return "show";
     }
 
 }
--- src/main/java/pl/szczerbiak/springLorem/model/LoremGenerator.java
+++ src/main/java/pl/szczerbiak/springLorem/model/LoremGenerator.java
@@ -1,5 +1,8 @@
 package pl.szczerbiak.springLorem.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -8,6 +11,33 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.joining;
 
 public class LoremGenerator {
+
+    /**
+     * Maximum number of paragraph to be generated
+     * Number of words and sentences is restriced by the size of the template
+     */
+    private final int MAX_NUMBER = 10;
+
+    @Pattern(regexp = "[0-9]+", message = "Input must be an integer")
+    private String number;
+    private String type;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     private static final String template = "Lorem ipsum dolor sit amet, " +
             "consectetur adipiscing elit, sed do eiusmod tempor " +
             "incididunt ut labore et dolore magna aliqua. Turpis " +
@@ -24,37 +54,31 @@ public class LoremGenerator {
         return template;
     }
 
+    // Private constructor: no one should ever make an instance of this class
     private LoremGenerator() {
     }
 
-    public static String generateText(int n, String type) {
+    public String generateText() {
+        // conversion into int
+        // we now that it is of the format [0-9]+
+        int n = Integer.parseInt(number);
         String text = "";
         switch (type) {
             case "paragraph":
                 text = Stream.generate(() -> getTemplate())
-                        .limit(n)
-                        .collect(joining("\n"));
+                        .limit(n <= MAX_NUMBER ? n : MAX_NUMBER)
+                        .collect(joining("\n")); // TODO: doesnt work
                 break;
             case "sentence":
                 String sentences[] = getTemplate().split("\\.");
-                // Functional programming
                 text = Arrays.stream(sentences).limit(n)
-                        .collect(joining("."));
-                // Old school
-//                for (int i = 0; i < n; i++) {
-//                    text += sentences[i] + ". ";
-//                }
+                        .collect(joining(".","","."));
                 break;
             case "word":
                 String words[] = getTemplate().split("\\s");
-                // Functional programming
                 text = Arrays.stream(words).limit(n)
                         .map(i -> i.replace(",", "").replace(".", ""))
                         .collect(joining(" "));
-                // Old school
-//                for (int i = 0; i < n; i++) {
-//                    text += words[i].replace(",", "").replace(".", "") +" ";
-//                }
                 break;
         }
         return text;
