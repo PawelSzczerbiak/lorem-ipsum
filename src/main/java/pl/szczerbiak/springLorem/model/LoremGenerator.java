8a2eb78de1de12b79be379d20213be76a9bc17a1
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2018-03-02 10:55:39 +0100

    Improvments in LoremGenerator class

--- src/main/java/pl/szczerbiak/springLorem/model/LoremGenerator.java
+++ src/main/java/pl/szczerbiak/springLorem/model/LoremGenerator.java
@@ -8,12 +8,6 @@ import static java.util.stream.Collectors.joining;
 
 public class LoremGenerator {
 
-    /**
-     * Maximum number of paragraph to be generated
-     * Number of words and sentences is restricted by the size of the template
-     */
-    private final int MAX_NUMBER = 10;
-
     @Pattern(regexp = "[0-9]+", message = "Input must be a positive integer")
     private String number;
     private Type type;
@@ -34,7 +28,22 @@ public class LoremGenerator {
         this.type = type;
     }
 
-    private static final String template = "Lorem ipsum dolor sit amet, " +
+    // Private constructor: no one should ever make an instance of this class
+    private LoremGenerator() {
+    }
+
+    /**
+     * Maximum number of paragraph to be generated
+     */
+    private final int PARAGRAPHS_MAX_NUMBER = 10;
+    /**
+     * Number of words and sentences is restricted
+     * by the size of the paragraph times the maximum number of paragraph
+     */
+    private final int SENTENCES_IN_PARAGRAPH = 11; // TODO: calculate dynamically
+    private final int WORDS_IN_PARAGRAPH = 95;  // TODO: calculate dynamically
+
+    private static final String PARAGRAPH = "Lorem ipsum dolor sit amet, " +
             "consectetur adipiscing elit, sed do eiusmod tempor " +
             "incididunt ut labore et dolore magna aliqua. Turpis " +
             "nunc eget lorem dolor. Vitae auctor eu augue ut lectus " +
@@ -46,38 +55,69 @@ public class LoremGenerator {
             "ut morbi tincidunt augue interdum. Neque ornare aenean euismod " +
             "elementum. Vitae suscipit tellus mauris a diam maecenas sed enim.";
 
-    public static String getTemplate() {
-        return template;
+    public static String getParagraph() {
+        return PARAGRAPH;
     }
 
-    // Private constructor: no one should ever make an instance of this class
-    private LoremGenerator() {
-    }
-
+    /**
+     * @return text that will be displayed on the screen
+     */
     public String generateText() {
-        // conversion to int
-        // we know that it is of the format [0-9]+
+        /**
+         * Conversion to int
+         * We know that it is of the format [0-9]+
+         */
         int n = Integer.parseInt(number);
         String text = "";
+        // The only special case that matters
+        if (n == 0) {
+            return text;
+        }
         switch (type) {
             case PARAGRAPH:
                 // NOTE: text must be wrapped by <p></p>
-                text = Stream.generate(() -> getTemplate())
-                        .limit(n <= MAX_NUMBER ? n : MAX_NUMBER)
+                text = Stream.generate(() -> getParagraph())
+                        .limit(n < PARAGRAPHS_MAX_NUMBER ? n : PARAGRAPHS_MAX_NUMBER)
                         .collect(joining("</p><p>"));
                 break;
             case SENTENCE:
-                String sentences[] = getTemplate().split("\\.");
+                String sentences[] = getSentenceTemplate(n).split("\\.");
                 text = Arrays.stream(sentences).limit(n)
-                        .collect(joining(".","","."));
+                        .collect(joining(".", "", "."));
                 break;
             case WORD:
-                String words[] = getTemplate().split("\\s");
+                String words[] = getWordTemplate(n).split("\\s");
                 text = Arrays.stream(words).limit(n)
-                        .map(i -> i.replace(",", "").replace(".", ""))
+                        .map(i -> i.replace(",", "")
+                                .replace(".", "")
+                                .toLowerCase())
                         .collect(joining(" "));
                 break;
         }
         return text;
+    }
+
+    /**
+     * @param n number of sentences to be displayed
+     * @return number of paragraphs containing n
+     */
+    private String getSentenceTemplate(int n) {
+        final int SENTENCES_MAX_NUMBER = PARAGRAPHS_MAX_NUMBER * SENTENCES_IN_PARAGRAPH;
+        return Stream.generate(() -> getParagraph())
+                .limit(n < SENTENCES_MAX_NUMBER ?
+                        (int) Math.ceil(1. * n / SENTENCES_IN_PARAGRAPH) : PARAGRAPHS_MAX_NUMBER)
+                .collect(joining(" "));
+    }
+
+    /**
+     * @param n number of words to be displayed
+     * @return number of paragraphs containing n
+     */
+    private String getWordTemplate(int n) {
+        final int WORD_MAX_NUMBER = PARAGRAPHS_MAX_NUMBER * WORDS_IN_PARAGRAPH;
+        return Stream.generate(() -> getParagraph())
+                .limit(n < WORD_MAX_NUMBER ?
+                        (int) Math.ceil(1. * n / WORDS_IN_PARAGRAPH) : PARAGRAPHS_MAX_NUMBER)
+                .collect(joining(" "));
     }
 }
